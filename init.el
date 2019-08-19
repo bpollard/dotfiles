c1f824610470247c9ddf028ba2860441a2b4fbe7
Brandon Pollard <bpollard@forensiclogic.com>
2019-08-19 15:54:42 -0700

    adding my newest version

--- init.el
+++ init.el
@@ -1,160 +1,47 @@
-;; Package Installation
 
-(require 'package)
-
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(defvar my-Packages
-  '(doom-themes
-    magit
-    go-mode
-    nyan-mode
-    auto-complete
-    company
-    company-go))
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")t)
+  (add-to-list
+   'package-archives
+   '("org" . "https://orgmode.org/elpa/") t))
 
-(mapc #'(lambda (package)
-	  (unless (package-installed-p package)
-	    (package-install lpackage)))
-	  my-Packages)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; My Customization
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(eval-when-compile
+  (require 'use-package))
 
-(require 'company)
-(require 'company-go)
-(ac-config-default)
-
-(setq company-tooltip-limit 20)
-(setq company-idle-delay .3)
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)))
-
-(add-hook 'python-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-jedi))
-                          (company-mode)))
-
-(global-set-key (kbd "M-o") 'other-window)
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
- '(backup-by-copying t)
- '(backup-directory-alist (quote (("." . "~/.emacs.d/saves"))))
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(current-language-environment "UTF-8")
- '(custom-enabled-themes (quote (doom-one)))
- '(custom-safe-themes
+ '(doom-dracula-brighter-comments t)
+ '(doom-dracula-brighter-modeline t)
+ '(doom-dracula-colorful-headers t)
+ '(doom-dracula-comment-bg t)
+ '(nyan-animate-nyancat t)
+ '(nyan-start-animation nil t)
+ '(nyan-toggle-wavy-trail t t)
+ '(package-selected-packages
    (quote
-    ("ee20facd6d54b7e1052e86dc2927546d9d103d4ad521e9e6cb2642ecb3f21208" "0598de4cc260b7201120b02d580b8e03bd46e5d5350ed4523b297596a25f7403" "90bd0eb20a1cb155b5a076f698b3c72cfe775aa7ea93b7bfbc171eb250db5e20" "a8c927cf1acf19ae27bd971894fa94c8114c31496c3d9cdfcc4d373dab34e4ef" "3d20cf0dbc6465a02c468abf2d9b8c17e88b20fbc05a04205a829285da28799d" "3edbdd0ad45cb8f7c2575c0ad8f6625540283c6e928713c328b0bacf4cfbb60f" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
- '(doom-one-brighter-comments t)
- '(doom-one-brighter-modeline t)
- '(doom-one-comment-bg t)
- '(doom-spacegrey-brighter-comments t)
- '(doom-spacegrey-brighter-modeline t)
- '(doom-spacegrey-comment-bg t)
- '(electric-pair-mode t)
- '(fci-rule-color "#073642")
- '(global-linum-mode t)
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
- '(ido-enable-flex-matching t)
- '(ido-everywhere t)
- '(ido-mode (quote both) nil (ido))
- '(indent-tabs-mode nil)
- '(jdee-db-active-breakpoint-face-colors (cons "#11171D" "#5EC4FF"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#11171D" "#8BD49C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#11171D" "#41505E"))
- '(magit-diff-use-overlays nil)
- '(menu-bar-mode nil)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(nyan-mode t)
- '(org-agenda-files
-   (quote
-    ("")))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(scroll-bar-mode nil)
- '(show-trailing-whitespace t)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(tool-bar-mode nil)
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c37300")
-     (60 . "#b97d00")
-     (80 . "#b58900")
-     (100 . "#a18700")
-     (120 . "#9b8700")
-     (140 . "#948700")
-     (160 . "#8d8700")
-     (180 . "#859900")
-     (200 . "#5a942c")
-     (220 . "#439b43")
-     (240 . "#2da159")
-     (260 . "#16a870")
-     (280 . "#2aa198")
-     (300 . "#009fa7")
-     (320 . "#0097b7")
-     (340 . "#008fc7")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(vc-make-backup-files t)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+    (yasnippet-snippets helm pytest org company-mode company-ansible flycheck doom-themes beacon nyan-mode ace-window company-terraform elpy magit which-key org-bullets use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 108 :width normal :foundry "DAMA" :family "Source Code Pro for Powerline")))))
+ '(default ((t (:inherit nil :stipple nil :foreground "#ccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Source Code Pro")))))
