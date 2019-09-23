2e4a0702c1f25f4de6ab6cbf5a7396747e84462b
Brandon Pollard <bpollard@trhc.com>
2019-09-23 07:52:35 -0700

    configuration for terraform, ansible and helm

--- init.el
+++ init.el
@@ -29,16 +29,49 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(custom-enabled-themes (quote (dracula)))
+ '(custom-safe-themes
+   (quote
+    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
  '(doom-dracula-brighter-comments t)
  '(doom-dracula-brighter-modeline t)
  '(doom-dracula-colorful-headers t)
  '(doom-dracula-comment-bg t)
+ '(fci-rule-color "#6272a4")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
  '(nyan-animate-nyancat t)
  '(nyan-start-animation nil t)
  '(nyan-toggle-wavy-trail t t)
+ '(objed-cursor-color "#ff5555")
  '(package-selected-packages
    (quote
-    (htmlize ox-reveal yasnippet-snippets helm pytest org company-mode company-ansible flycheck doom-themes beacon nyan-mode ace-window company-terraform elpy magit which-key org-bullets use-package))))
+    (terraform ansible-docs ansible-doc jinja2-mode yaml-mode dracula-theme htmlize ox-reveal yasnippet-snippets helm pytest org company-mode company-ansible flycheck doom-themes beacon nyan-mode ace-window company-terraform elpy magit which-key org-bullets use-package)))
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50fa7b")
+    (cons 40 "#85fa80")
+    (cons 60 "#bbf986")
+    (cons 80 "#f1fa8c")
+    (cons 100 "#f5e381")
+    (cons 120 "#face76")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ffa38a")
+    (cons 180 "#ff8ea8")
+    (cons 200 "#ff79c6")
+    (cons 220 "#ff6da0")
+    (cons 240 "#ff617a")
+    (cons 260 "#ff5555")
+    (cons 280 "#d45558")
+    (cons 300 "#aa565a")
+    (cons 320 "#80565d")
+    (cons 340 "#6272a4")
+    (cons 360 "#6272a4")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
